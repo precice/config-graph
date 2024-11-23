a1f0ba09c8113598cd691b4bdcf3d986e9adf9a2
Orlando <orlac2700@gmail.com>
2024-11-23 07:24:50 +0100

    Changed nodes.py.

--- preciceconfiggraph/nodes.py
+++ preciceconfiggraph/nodes.py
@@ -1,10 +1,4 @@
-class Node:
-    def __init__(self, name):
-        self.name = name
-
-
-class ParticipantNode(Node):
-    # TODO new attribute socket-connected participant?
+class ParticipantNode:
     def __init__(self, name, write_data, read_data, receive_mesh, provide_mesh, mapping):
         self.name = name
         self.write_data = write_data
@@ -14,15 +8,14 @@ class ParticipantNode(Node):
         self.mapping = mapping
 
 
-class MeshNode(Node):
-    # TODO dimensions attribute?
+class MeshNode:
     def __init__(self, name, use_data, write_data):
         self.name = name
         self.use_data = use_data
         self.write_data = write_data
 
 
-class CouplingNode(Node):
+class CouplingNode:
     def __init__(self, name, first_participant, second_participant, data, mesh, from_participant, to_participant):
         self.name = name
         self.first_participant = first_participant
@@ -33,41 +26,36 @@ class CouplingNode(Node):
         self.to_participant = to_participant
 
 
-class DataNode(Node):
-    # TODO used_by_participants as array? used_by_mesh also array? used_by_mapping also array?
-    #  used_by_coupling/exchange
+class DataNode:
     def __init__(self, name):
         self.name = name
 
 
-class MappingNode(Node):
-    def __init__(self, name, direction, from_participant, to_participant):
+class MappingNode:
+    def __init__(self, name, parent, direction, from_participant, to_participant):
         self.name = name
+        self.parent = parent
         self.direction = direction
         self.from_participant = from_participant
         self.to_participant = to_participant
 
 
-class WriteDataNode(Node):
-    # TODO is name needed here? name or data, one is redundant
-    def __init__(self, name, participant, data, mesh):
-        self.name = name
+class WriteDataNode:
+    def __init__(self, participant, data, mesh):
         self.participant = participant
         self.data = data
         self.mesh = mesh
 
-class ReadDataNode(Node):
-    # TODO is name needed here? name or data, one is redundant
-    def __init__(self, name, participant, data, mesh):
-        self.name = name
+
+class ReadDataNode:
+    def __init__(self, participant, data, mesh):
         self.participant = participant
         self.data = data
         self.mesh = mesh
 
-class ExchangeNode(Node):
-    # TODO is name needed here? name or data, one is redundant
-    def __init__(self, name, coupling_scheme, data, from_participant, to_participant ):
-        self.name = name
+
+class ExchangeNode:
+    def __init__(self, coupling_scheme, data, from_participant, to_participant):
         self.coupling_scheme = coupling_scheme
         self.data = data
         self.from_participant = from_participant
