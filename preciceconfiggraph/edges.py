2344b3fff9922f9057e075fa44cc4a31da9d6f2e
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-03 23:20:27 +0100

    redo edges to include the types of connections

--- preciceconfiggraph/edges.py
+++ preciceconfiggraph/edges.py
@@ -2,14 +2,60 @@ from enum import Enum
 
 
 class Edge(Enum):
-    RECEIVE_MESH = "receive-mesh"
-    PROVIDE_MESH = "provide-mesh"
+    # mesh --received-by-> receive-mesh
+    RECEIVE_MESH__MESH_RECEIVED_BY = "receive-mesh_mesh-received-by"
+    # participant (from) --received-by-> receive-mesh
+    RECEIVE_MESH__PARTICIPANT_RECEIVED_BY = "receive-mesh_participant-received-by"
+    # The connection between receive-mesh and participant it is part of
+    RECEIVE_MESH__CHILD_OF = "receive-mesh_child-of"
 
-    MAPPING = "mapping"
-    EXCHANGE = "exchange"
+    # participant --provides-> mesh
+    PROVIDE_MESH__PARTICIPANT_PROVIDES = "provide-mesh_participant-provides"
+
+
+    # mapping --to-> mesh
+    MAPPING__TO_MESH = "mapping_to-mesh"
+    # mesh --mapped-by-> mapping
+    MAPPING__MESH_MAPPED_BY = "mapping_mesh-mapped-by"
+    # The connection between mapping and participant it is part of
+    MAPPING__PARTICIPANT_PARENT_OF = "mapping_participant-parent-of"
+    MAPPING__CHILD_OF = "mapping_child-of"
+
+    # participant (from) --exchanged_by--> exchange
+    EXCHANGE__PARTICIPANT_EXCHANGED_BY = "exchange_participant-exchanged-by"
+    # exchange --exchanges-to--> participant (to)
+    EXCHANGE__EXCHANGES_TO = "exchange_exchanges-to"
+    # exchange <--> data
+    EXCHANGE__DATA = "exchange_data"
+    # exchange <--> mesh
+    EXCHANGE__MESH = "exchange_mesh"
+    # The connection between receive-mesh and participant it is part of
+    EXCHANGE__CHILD_OF = "exchange_child-of"
+    EXCHANGE__COUPLING_SCHEME_PARENT_OF = "exchange_coupling-scheme-parent-of"
+
+    # participant (connector) -> participant (acceptor)
     SOCKET = "socket"
-    COUPLING_SCHEME = "coupling-scheme"
+
+    # participant (first) <--> coupling-scheme
+    COUPLING_SCHEME__PARTICIPANT_FIRST = "coupling-scheme_participant-first"
+    # participant (second) <--> coupling-scheme
+    COUPLING_SCHEME__PARTICIPANT_SECOND = "coupling-scheme_participant-second"
+
 
     USE_DATA = "use-data"
-    WRITE_DATA = "write-data"
-    READ_DATA = "read-data"
+
+    # write-data --writes-to-> data
+    WRITE_DATA__WRITES_TO_DATA = "write-data_writes-to-data"
+    # write-data --writes-to-> mesh
+    WRITE_DATA__WRITES_TO_MESH = "write-data_writes-to-mesh"
+    # The connection between write-data and participant it is part of
+    WRITE_DATA__PARTICIPANT_PARENT_OF = "write-data_participant-parent-of"
+    WRITE_DATA__CHILD_OF = "write-data_child-of"
+
+    # data --read-by-> read-data
+    READ_DATA__DATA_READ_BY = "read-data_data-read-by"
+    # mesh --read-by-> read-data
+    READ_DATA__MESH_READ_BY = "read-data_mesh-read-by"
+    # The connection between read-data and participant it is part of
+    READ_DATA__PARTICIPANT_PARENT_OF = "read-data_participant-parent-of"
+    READ_DATA__CHILD_OF = "read-data_child-of"
