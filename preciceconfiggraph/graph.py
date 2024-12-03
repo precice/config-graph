1ab6e62dbab9f11655ce11f1b77dca855d2c7005
uowis <mail@example.com>
2024-12-03 13:38:27 +0100

    apply linting suggestions

--- preciceconfiggraph/graph.py
+++ preciceconfiggraph/graph.py
@@ -13,7 +13,7 @@ def get_graph(root: etree._Element) -> nx.DiGraph:
         for child in e.iterchildren():
             if child.tag.startswith(prefix):
                 postfix = child.tag[child.tag.find(":") + 1 :]
-                yield (child, postfix)
+                yield child, postfix
 
 
     # FIND NODES
@@ -73,7 +73,7 @@ def get_graph(root: etree._Element) -> nx.DiGraph:
             mesh_name = write_data_el.attrib['mesh'] # TODO: Error on not found
             mesh = mesh_nodes[mesh_name] # TODO: Raise custom error if mesh not found
 
-            write_data = n.WriteDataNode(participant, data, mesh) # FIXME: participant is not yet initalized
+            write_data = n.WriteDataNode(participant, data, mesh) # FIXME: participant is not yet initialized
             participant.write_data.append(write_data)
             write_data_nodes.append(write_data)
 
@@ -85,7 +85,7 @@ def get_graph(root: etree._Element) -> nx.DiGraph:
             mesh_name = read_data_el.attrib['mesh'] # TODO: Error on not found
             mesh = mesh_nodes[mesh_name] # TODO: Raise custom error if mesh not found
 
-            read_data = n.ReadDataNode(participant, data, mesh) # FIXME: participant is not yet initalized
+            read_data = n.ReadDataNode(participant, data, mesh) # FIXME: participant is not yet initialized
             participant.read_data.append(read_data)
             read_data_nodes.append(read_data)
         
