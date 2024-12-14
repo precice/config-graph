abb96fc80c5697ccf5cf77b2a66f1d74f4e910b3
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2024-12-14 18:07:47 +0100

    refactor lib to out of the library

--- precice_config_graph/lib.py
+++
@@ -1,8 +0,0 @@
-from . import graph as g
-from .xml_processing import parse_file
-
-
-if __name__ == "__main__":
-    root = parse_file("test/example-configs/simple-good/precice-config.xml")
-    G = g.get_graph(root)
-    g.print_graph(G)
