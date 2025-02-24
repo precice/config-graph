4999d95fa5b7fa02af5f46ff166bd055a7860826
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-02-24 14:15:11 +0100

    Fix parsing boolean

--- precice_config_graph/xml_processing.py
+++ precice_config_graph/xml_processing.py
@@ -14,3 +14,11 @@ def parse_file(path: str) -> etree._Element:
     parser = etree.XMLParser(recover=True, remove_comments=True)
     tree = etree.fromstring(open(path, "rb").read(), parser)
     return tree
+
+# https://github.com/precice/precice/blob/2819cd61b747c35ae5c8ddd78866e913144c6ca4/src/utils/String.cpp#L56-L64
+def convert_string_to_bool(string: str) -> bool:
+    string = string.lower()
+    if string == "1" or string == "yes" or string == "true" or string == "on":
+        return True
+
+    return False
