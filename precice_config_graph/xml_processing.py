60a06f69f38bf1128d672c373cfd3be8d210b949
s3n-w6i <37022952+s3n-w6i@users.noreply.github.com>
2025-02-24 14:23:58 +0100

    Add more comments

--- precice_config_graph/xml_processing.py
+++ precice_config_graph/xml_processing.py
@@ -15,6 +15,8 @@ def parse_file(path: str) -> etree._Element:
     tree = etree.fromstring(open(path, "rb").read(), parser)
     return tree
 
+# Function to get a boolean value from a string
+# Implemented according to how preCICE itself implements this method:
 # https://github.com/precice/precice/blob/2819cd61b747c35ae5c8ddd78866e913144c6ca4/src/utils/String.cpp#L56-L64
 def convert_string_to_bool(string: str) -> bool:
     string = string.lower()
