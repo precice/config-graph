4cad7897cc542209e870e0bec886f8319eafd23d
Orlando <orlac2700@gmail.com>
2026-01-25 11:08:27 +0100

    New test for named comparison

--- tests/graph-equivalence/graph_equivalence_test.py
+++ tests/graph-equivalence/graph_equivalence_test.py
@@ -21,3 +21,13 @@ def test_graph_equivalence_false():
     path_to_actual: str = "tests/graph-equivalence/not-equivalent/actual-precice-config.xml"
     assert not graph.check_config_equivalence(path_to_expected, path_to_actual,
                                               ignore_names=True), "Graphs are equivalent."
+
+
+def test_graph_equivalence_names_false():
+    """
+    Tests if graph.py's check_config_equivalence function works as expected when including names.
+    The configs are expected to not be equivalent.
+    """
+    path_to_expected: str = "tests/graph-equivalence/not-equivalent/expected-precice-config.xml"
+    path_to_actual: str = "tests/graph-equivalence/not-equivalent/names_actual-precice-config.xml"
+    assert not graph.check_config_equivalence(path_to_expected, path_to_actual), "Graphs are equivalent."
